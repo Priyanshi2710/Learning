4f714f8a762d53344050c798132b6dc1cd85bac7
Priyanshi2710 <priyanshi.dave@triveniglobalsoft.com>
2023-06-27 17:12:41 +0530

    SignalR implimentation complete.

---
+++ DemoAppSignalR/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DemoAppSignalR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IHubContext<MessageHub> _hubContext;
+        public HomeController(IHubContext<MessageHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(string user, string message)
+        {
+            _hubContext.Clients.All.SendAsync("ReceiveMessage", user, message);
+            return Ok();
+        }
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+
+    }
+}
